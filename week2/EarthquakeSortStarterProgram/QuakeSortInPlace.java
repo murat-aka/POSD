bf6b5c0a5155dd5417ca0cab457048c48f0c3127
murat-aka <murat-aka@hotmail.com>
2015-12-30 17:04:36 +0000

    finished selectionsort and add exercise sorting at scale

--- week2/EarthquakeSortStarterProgram/QuakeSortInPlace.java
+++ week2/EarthquakeSortStarterProgram/QuakeSortInPlace.java
@@ -89,7 +89,7 @@ public class QuakeSortInPlace
     
     public void sortByLargestDepth(ArrayList<QuakeEntry> in) {
        
-       for (int i=0; i< in.size(); i++) {
+       for (int i=0; i< 70; i++) {
             int minIdx = getLargestDepth(in,i);
             QuakeEntry qi = in.get(i);
             QuakeEntry qmin = in.get(minIdx);
@@ -125,7 +125,8 @@ public class QuakeSortInPlace
         EarthQuakeParser parser = new EarthQuakeParser(); 
         //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
         //String source = "data/nov20quakedatasmall.atom";
-        String source = "data/earthquakeDataSampleSix2.atom";
+        //String source = "data/earthquakeDataSampleSix.atom";
+        String source = "data/earthQuakeDataDec6sample2.atom";
         //String source = "data/nov20quakedata.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);  
        
@@ -133,8 +134,8 @@ public class QuakeSortInPlace
         //sortByMagnitude(list);
         //sortByLargestDepth(list);
         //sortByMagnitudeWithBubbleSort(list);
-        //sortByMagnitudeWithBubbleSortWithCheck(list);
-         sortByMagnitudeWithCheck(list);
+        sortByMagnitudeWithBubbleSortWithCheck(list);
+        // sortByMagnitudeWithCheck(list);
         for (QuakeEntry qe: list) { 
            // System.out.println(qe);
         } 
